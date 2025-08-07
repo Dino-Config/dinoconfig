ebddeff24fb77b80ebcdd2176f3585d3bc76f673
stanisev <shadowrageinc@gmail.com>
2025-08-07 23:17:47 +0300

    [FE] Generate main page

--- apps/dinoconfig-fe/src/app/app.config.ts
+++ apps/dinoconfig-fe/src/app/app.config.ts
@@ -6,12 +6,14 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations()
   ],
 };
--- apps/dinoconfig-fe/src/app/app.ts
+++ apps/dinoconfig-fe/src/app/app.ts
@@ -2,18 +2,33 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { JsonPipe } from '@angular/common';
 import { environment } from '../environments/environment';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @Component({
-  imports: [RouterModule, JsonPipe],
+  imports: [
+    RouterModule, 
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSlideToggleModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.scss'
 })
 export class App {
   private httpClient = inject(HttpClient);
   private api = this.httpClient.get(environment.apiUrl);
-  
+
   protected apiResponse = toSignal(this.api);
 }
