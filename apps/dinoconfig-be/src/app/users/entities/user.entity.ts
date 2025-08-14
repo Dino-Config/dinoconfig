713f1ea3fe85906cdc43eef80c99ff235dfe0ca6
darkoandreev <andreev.darko@gmail.com>
2025-08-14 19:05:37 +0300

    add config, user, brand entities, controllers, services

---
+++ apps/dinoconfig-be/src/app/users/entities/user.entity.ts
@@ -0,0 +1,48 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany, Index } from 'typeorm';
+import { Brand } from '../../brands/entities/brand.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ length: 100 })
+  firstName: string;
+
+  @Column({ length: 100 })
+  lastName: string;
+
+  @Index({ unique: true })
+  @Column({ type: 'citext' }) // Make sure citext extension is enabled in Postgres
+  email: string;
+
+  @Column({ length: 20, nullable: true })
+  phoneNumber?: string;
+
+  @Column({ type: 'text', nullable: true })
+  address?: string;
+
+  @Column({ length: 100, nullable: true })
+  city?: string;
+
+  @Column({ length: 100, nullable: true })
+  state?: string;
+
+  @Column({ length: 20, nullable: true })
+  zip?: string;
+
+  @Column({ length: 100, nullable: true })
+  country?: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ length: 255, nullable: true })
+  companyName?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => Brand, brand => brand.user)
+  brands: Brand[];
+}
