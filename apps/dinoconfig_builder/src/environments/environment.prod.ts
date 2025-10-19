0e227f9bbb2dd1bc7ed26e7e29ba434987b1fc52
darkoandreev <andreev.darko@gmail.com>
2025-10-19 22:48:16 +0300

    fix build

--- apps/dinoconfig_builder/src/environments/environment.prod.ts
+++ apps/dinoconfig_builder/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     apiUrl: process.env.NX_PUBLIC_BUILDER_DINOCONFIG_API_URL,
-    homeUrl: process.env.NX_PUBLIC_BUILDER_DINOCONFIG_HOME_URL,
+    homeUrl: process.env.NX_PUBLIC_BUILDER_DINOCONFIG_HOME_URL || '',
     stripePublishableKey: process.env.NX_PUBLIC_BUILDER_STRIPE_PUBLISHABLE_KEY,
     stripeFreePriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_FREE_PRICE_ID,
     stripeStarterPriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_STARTER_PRICE_ID,
