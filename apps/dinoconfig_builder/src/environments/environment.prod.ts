9aa5ae2c8e86724d64f3e124d0566bd58f04bd46
darkoandreev <andreev.darko@gmail.com>
2025-10-19 22:44:37 +0300

    add prod env variables for the builder

--- apps/dinoconfig_builder/src/environments/environment.prod.ts
+++ apps/dinoconfig_builder/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    apiUrl: 'https://api.dinoconfig.com/api',
-    homeUrl: 'https://dinoconfig.com',
-    stripePublishableKey: 'pk_test_51SIxsxH7N4M4kaOnCYAouH8sfNEt7LxwBDhZvK5x8i1CkqLgFcw3m9xwdxFcoCJBD7DQ2dWPzxgiQ6rEc7oos8cD00UkrauFkk',
-    stripeFreePriceId: 'price_1SIxx6H7N4M4kaOnXcpEG3h3',
-    stripeStarterPriceId: 'price_1SIxxUH7N4M4kaOnOnYnEIsH',
-    stripeProPriceId: 'price_1SIxxmH7N4M4kaOn4uafT7ey',
-    stripeCustomPriceId: 'price_custom_replace_me'
+    apiUrl: process.env.NX_PUBLIC_BUILDER_DINOCONFIG_API_URL,
+    homeUrl: process.env.NX_PUBLIC_BUILDER_DINOCONFIG_HOME_URL,
+    stripePublishableKey: process.env.NX_PUBLIC_BUILDER_STRIPE_PUBLISHABLE_KEY,
+    stripeFreePriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_FREE_PRICE_ID,
+    stripeStarterPriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_STARTER_PRICE_ID,
+    stripeProPriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_PRO_PRICE_ID,
+    stripeCustomPriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_CUSTOM_PRICE_ID
 };
