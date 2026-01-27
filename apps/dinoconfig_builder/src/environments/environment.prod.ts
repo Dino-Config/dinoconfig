fa01a3222de2da3f38e0facebf80d2548aab6248
darkoandreev <andreev.darko@gmail.com>
2026-01-27 21:22:04 +0200

    remove dinoconfig_builder react

--- apps/dinoconfig_builder/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    apiUrl: process.env.NX_PUBLIC_BUILDER_DINOCONFIG_API_URL,
-    homeUrl: process.env.NX_PUBLIC_BUILDER_DINOCONFIG_HOME_URL || '',
-    stripePublishableKey: process.env.NX_PUBLIC_BUILDER_STRIPE_PUBLISHABLE_KEY,
-    stripeFreePriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_FREE_PRICE_ID,
-    stripeStarterPriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_STARTER_PRICE_ID,
-    stripeProPriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_PRO_PRICE_ID,
-    stripeCustomPriceId: process.env.NX_PUBLIC_BUILDER_STRIPE_CUSTOM_PRICE_ID
-};
